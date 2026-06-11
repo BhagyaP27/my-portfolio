1b46978168e893e203c74a810094d25e3c0b5303
Bhagya Patel <bhagyapatel2077@gmail.com>
2026-06-11 01:03:40 -0400

    updated packages to deploy via Github pages, but i think ill use Vercel because it auto redploys every push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//Import HashRouter instead of BrowserRouter
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
@@ -9,6 +10,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
+    //Use HashRouter (aliased as Router here)
     <Router>
       <div className="App">
         <Navbar />
